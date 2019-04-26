4d52cd13ee803dc1fdf3121f9c66efefe3d86066
齋藤慧楽 <sarakichi0425@gmail.com>
2019-04-26 17:45:28 +0900

    ScriptTest lesson5課題:Pinballにスコア追加
    小さい星は1点、大きい星は5点、小さい雲は10点、大きい雲は20点

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+	private GameObject scoreText;
+	private int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		this.scoreText = GameObject.Find("Score");
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		string textscore = this.score.ToString();
+		
+		if (collision.gameObject.tag == "SmallStarTag") {
+			//スコア処理を追加
+			this.score +=1;
+			this.scoreText.GetComponent<Text>().text= textscore;
+
+		}
+		if (collision.gameObject.tag == "LargeStarTag") {
+			//スコア処理を追加
+			this.score +=5;
+			this.scoreText.GetComponent<Text>().text= textscore;
+
+		}
+		if (collision.gameObject.tag == "SmallCloudTag") {
+			//スコア処理を追加
+			this.score +=10;
+			this.scoreText.GetComponent<Text>().text= textscore;
+
+		}
+		if (collision.gameObject.tag == "LargeCloudTag") {
+			//スコア処理を追加
+			this.score +=20;
+			this.scoreText.GetComponent<Text>().text= textscore;
+
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
